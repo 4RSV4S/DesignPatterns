7245098fffc6adad4d1d6caa9c73cd9cce46d494
Arseni Vasenda <vasenda2000@yandex.ru>
2022-02-01 03:32:49 +0300

    Add project files

---
+++ Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var h1 = UserHelp.GetInstance();
+            var h2 = UserHelp.GetInstance();
+            bool areEqual = ReferenceEquals(h1, h2);
+
+            if(areEqual == true)
+            {
+                Console.WriteLine("Singleton class 'UserHelp' works, both variables references refer to the same object.");
+                Console.WriteLine(UserHelp.GetHelp());
+            }
+            else if(areEqual == false)
+            {
+                Console.WriteLine("Singleton class 'UserHelp' failed, both variables references refer to the different objects.");
+            }
+        }
+    }
+}
---
+++ Singleton/UserHelp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class UserHelp
+    {
+        private UserHelp() { }
+
+        private static UserHelp _instance;
+
+        public static UserHelp GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new UserHelp();
+            }
+            return _instance;
+        }
+
+        public static string GetHelp()
+        {
+            return "Some user help";
+        }
+    }
+}
